2057ee3111aa250831de50c29bb75db5fb9c7358
Vinícius dos Santos Rocha <vinicius.santosrocha10@gmail.com>
2026-09-22 13:48:26 -0300

    feat(network): connectivity service

---
+++ lib/core/network/connectivity_service.dart
@@ -0,0 +1,27 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+/// Online/offline signal (OFFL-01, OFFL-02, RECALC-06).
+abstract class ConnectivityService {
+  /// Emits on every change, without repeating the same value.
+  Stream<bool> get isOnline;
+
+  Future<bool> check();
+}
+
+/// Online when any reported result is not [ConnectivityResult.none].
+class ConnectivityServiceImpl implements ConnectivityService {
+  ConnectivityServiceImpl(this._connectivity);
+
+  final Connectivity _connectivity;
+
+  @override
+  Stream<bool> get isOnline =>
+      _connectivity.onConnectivityChanged.map(_hasConnection).distinct();
+
+  @override
+  Future<bool> check() async =>
+      _hasConnection(await _connectivity.checkConnectivity());
+
+  static bool _hasConnection(List<ConnectivityResult> results) =>
+      results.any((result) => result != ConnectivityResult.none);
+}
